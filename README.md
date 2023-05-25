5eb06282ed62737fb8f00be86bfe0ab6f6ba7d7a
moemnml33 <mhmdbkaili@hotmail.com>
2023-05-25 13:38:52 -0400

    Second commit - the base grid

---
+++ README.md
@@ -0,0 +1,23 @@
+# Tetris game 
+
+The following is the recreation of the classic Tetris game with python using pygame, and the following is the game walkthrough.
+
+# About the commits, building the game step-by-step:
+
+## First commit - the game's infrastructure:
+
+In the first step, we're just laying the infrastructure of the game; creating an empty window in which the game will be run.
+
+![](images/first_commit.png)
+
+## Second commit - the base grid:
+
+In the second step, we're creating the base grid; using a draw_grid function, we iterate over the x's and y's of the window (columns and rows) and draw our grid where each block is made of a grey square. With the parameters the function takes, the grid dynamically adapts to any changes we make to the window size, number of rows and columns, and the grid size.
+
+![](images/second_commit.png)
+
+
+
+
+
+
--- Tetris.py
+++ Tetris.py
@@ -1,10 +1,24 @@
 import pygame
 from pygame.locals import *
-
 pygame.init()
 # create window
 screen = pygame.display.set_mode((500, 600))
 pygame.display.set_caption("Tetris")
+
+# draw grid function
+def draw_grid(cols, rows, grid_size, x_gap, y_gap):
+    for y in range(rows): # rows
+        for x in range(cols): # columns
+            # draw rect onto screen, colour grey, rectangle parameters, draw border only
+            pygame.draw.rect(screen, (100, 100, 100), 
+                             [x * grid_size + x_gap, y * grid_size + y_gap, grid_size, grid_size], 1)
+
+# grid size, cols, rows, x and y gaps variables to draw grid
+grid_size = 30
+cols = screen.get_width() // grid_size
+rows = screen.get_height() // grid_size
+x_gap = (screen.get_width() - cols * grid_size) // 2
+y_gap = (screen.get_height() - rows * grid_size) // 2
 
 # main game loop
 game_over = False
@@ -17,6 +31,10 @@ while not game_over:
         if event.type == pygame.QUIT:
             game_over = True
     screen.fill((0, 0, 0))
+    
+    # draw grid
+    draw_grid(cols, rows, grid_size, x_gap, y_gap)
+    
     # update display after each iteration
     pygame.display.update()
 
