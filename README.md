ab9932213b359c68ada98f927b6c48a28352f687
moemnml33 <mhmdbkaili@hotmail.com>
2023-05-25 15:16:45 -0400

    Third commit - defining the blocks and displaying shapes

--- README.md
+++ README.md
@@ -16,6 +16,20 @@ In the second step, we're creating the base grid; using a draw_grid function, we
 
 ![](images/second_commit.png)
 
+## Third commit - defining the blocks and displaying shapes:
+
+In the thrid step, we define each shape to be contained in a 3x3 matrix. For example, a straight shape can be represented as the matrix:
+[0 1 0  
+ 0 1 0
+ 0 1 0]
+where the 1's are the valid blocks, which can be represented as the list [1, 4, 7]. The same logic follows for the shape's rotation, and for all other shapes.
+Then we created a Block class, in which the contructor takes the coordinates as parameters, and specifies the shape and the shape variation that we want to pick. The shape function returns the shape from the list of blocks based on the values chosen in the constructor above.
+Lastly, we created a draw block function that loops in the 3x3 matrix and draws the desired shape on the screen.
+
+![](images/third_commit_drawing_shape_1.png)
+
+![](images/third_commit_drawing_shape_2.png)
+
 
 
 
--- Tetris.py
+++ Tetris.py
@@ -13,6 +13,46 @@ def draw_grid(cols, rows, grid_size, x_gap, y_gap):
             pygame.draw.rect(screen, (100, 100, 100), 
                              [x * grid_size + x_gap, y * grid_size + y_gap, grid_size, grid_size], 1)
 
+# defining the blocks
+# each shape is contained within a 3x3 matrix
+# each list contains the coordinates that this particular shape occupies in that matrix
+blocks = [
+    [[1, 4, 7], [3, 4, 5]],  # 0: straight shape
+    [[1, 3, 4, 5, 7]],  # 1: cross shape
+    [[0, 1, 4, 5], [1, 3, 4, 6]],  # 2: two on two 1 shape
+    [[1, 2, 3, 4], [0, 3, 4, 7]],  # 3: two on two 2 shape
+    [[0, 1, 3, 6], [0, 1, 2, 5], [2, 5, 7, 8], [3, 6, 7, 8]],  # 4: L 1 shape
+    [[1, 2, 5, 8], [5, 6, 7, 8], [0, 3, 6, 7], [0, 1, 2, 3]],  # 5: L 2 shape
+    [[4, 6, 7, 8], [0, 3, 4, 6], [0, 1, 2, 4], [2, 4, 5, 8]]  # 6: one on three
+]
+# block class
+class Block:
+    # constructor
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.type = 1   # select the shape from the blocks list
+        self.rotation = 0   # select the variation of the shape (ex: straight vs. rotated) from the blocks list
+    
+    # pick the shape from the list based on the values in the constructor above
+    def shape(self):
+        return blocks[self.type][self.rotation]
+
+# draw block function
+def draw_block():
+    # 3x3 matrix
+    for y in range(3):
+        for x in range(3):
+            if y * 3 + x in block.shape():
+                pygame.draw.rect(screen, (255, 255, 255), 
+                                 # x: location of block in its 3x3 matrix
+                                 # block.x: location that we want on the grid
+                                 # + 1 and - 2: so the block does not overlap with grid
+                                 [(x + block.x) * grid_size + x_gap + 1, 
+                                 (y + block.y) * grid_size + y_gap + 1,
+                                 grid_size - 2, 
+                                 grid_size - 2])
+
 # grid size, cols, rows, x and y gaps variables to draw grid
 grid_size = 30
 cols = screen.get_width() // grid_size
@@ -23,6 +63,8 @@ y_gap = (screen.get_height() - rows * grid_size) // 2
 # main game loop
 game_over = False
 clock = pygame.time.Clock()
+# block object to test
+block = Block(5, 6)
 while not game_over:
     # help limit the runtime speed of the game 
     # the program will never run at more than 50 frames per second. 
@@ -34,7 +76,8 @@ while not game_over:
     
     # draw grid
     draw_grid(cols, rows, grid_size, x_gap, y_gap)
-    
+    # draw block to test
+    draw_block()
     # update display after each iteration
     pygame.display.update()
 
